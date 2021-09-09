e66dad5d8388307ddc484d1003a1d21a6433e6fc
carolinecole25 <carolinecole25@gmail.com>
2021-09-09 20:11:53 +0000

    development settings

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 import dj_database_url
 
-development = os.environ.get('DEVELOPMENT' False)
+development = os.environ.get('DEVELOPMENT', False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-ysb$#&@ue+qkso1%5_pxbm0m-fk!4-s!169pq8kjeelgf07ge3')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
@@ -85,7 +85,7 @@ if development:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
         }
     }
 else:
